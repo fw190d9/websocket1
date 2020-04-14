b67702d8b44d90a2ccd686ca04c1e65f54f304fa
fw190d9 <fw190@hanmail.net>
2020-04-14 12:51:39 +0900

    first commit

---
+++ websocket01/src/main/java/fw190/websocket/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fw190.websocket;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Websocket01Application.class);
+	}
+
+}
---
+++ websocket01/src/main/java/fw190/websocket/Websocket01Application.java
@@ -0,0 +1,16 @@
+package fw190.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class Websocket01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websocket01Application.class, args);
+	}
+
+}
