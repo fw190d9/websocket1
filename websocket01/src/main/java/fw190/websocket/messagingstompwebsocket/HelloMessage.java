149778e50b79c9dfee3e2a71b98a966c39d7f543
fw190d9 <fw190@hanmail.net>
2020-04-14 15:30:46 +0900

    websocket commit (https://spring.io/guides/gs/messaging-stomp-websocket/)

---
+++ websocket01/src/main/java/fw190/websocket/messagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,20 @@
+package fw190.websocket.messagingstompwebsocket;
+
+public class HelloMessage {
+	private String name;
+
+	public HelloMessage() {
+	}
+
+	public HelloMessage(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
